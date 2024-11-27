e308a237932e5faeeb1284f38d1e35f460a503c4
Ghaliah <ghalia.banouna@gmail.com>
2024-11-27 13:33:46 +0300

    Home page Template and View

--- ArtGallery/main/views.py
+++ ArtGallery/main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
-# Create your views here.
+# Home View
+def homeView(request: HttpRequest):
+    
+    return render(request, 'main/index.html')
