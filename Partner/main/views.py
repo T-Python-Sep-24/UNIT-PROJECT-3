7f952fc4907d601d4bd71d5c17584f8bef5575a0
Areej Shareefi <aomsh1420@gmail.com>
2024-11-27 06:54:24 +0300

    home view and set up project

---
+++ Partner/main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+# Create your views here.
+
+
+def home_view(request:HttpRequest):
+    return render(request,"main/home.html")
