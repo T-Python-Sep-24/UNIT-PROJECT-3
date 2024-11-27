ab0df867a926373fc1008aa02242ae7ce8459694
Khalid ALMaghyadi <K95.job@gmail.com>
2024-11-28 00:32:57 +0300

    Project development in progress

--- AirportTuwaiq/main/views.py
+++ AirportTuwaiq/main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect 
+from django.http import HttpRequest , HttpResponse
+from django.contrib import messages
 # Create your views here.
+
+def home_view(request:HttpRequest):
+    messages.info(request, 'This is a global message.')
+    return render(request, 'main/home.html') 
--- AirportTuwaiq/users/views.py
+++ AirportTuwaiq/users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpRequest , HttpResponse
+from django.contrib import messages
 # Create your views here.
+
+def sign_up_view(request:HttpRequest):
+    pass 
