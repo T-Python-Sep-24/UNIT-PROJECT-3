865b332c7d7374101e5925243fd680e4bd0d22ae
HmzhDubh <meezo.2015@hotmail.com>
2024-11-28 00:35:01 +0300

    update home page

--- EchoFund/main/views.py
+++ EchoFund/main/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-
+from funds_app.models import Fund
 # Create your views here.
 
 def home_view(request: HttpRequest):
 
-    return render(request, 'index.html')
+    funds = Fund.objects.all()[:3]
+
+    return render(request, 'index.html', context={'funds': funds})
