103d09ee398a8e5e31875ec62c0ed8a0cfea1300
Ali <almaghamsiali@gmail.com>
2024-12-01 03:56:06 +0300

    changed the model ticket location

--- MovieSystem/movies/models.py
+++ MovieSystem/movies/models.py
@@ -30,3 +30,14 @@ class Screening(models.Model):
 
     def __str__(self):
         return f"{self.movie.title} at {self.showtime}"
+
+class Ticket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    screening = models.ForeignKey(Screening, on_delete=models.CASCADE)
+    seat_number = models.CharField(max_length=10)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    booked_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Ticket {self.id} for {self.screening.movie.title} by {self.user.username}"
+
