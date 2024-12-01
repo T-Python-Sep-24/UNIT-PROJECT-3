358dfd5d1631d6cab4c38fd475ae2975ea89ee50
AR <aarokan@gmail.com>
2024-12-01 03:05:00 +0300

    create the sujbects

--- PeerLearn/subjects/models.py
+++ PeerLearn/subjects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=256)
+    logo = models.ImageField(upload_to="images/", default="images/default.jpg")
+    about = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
