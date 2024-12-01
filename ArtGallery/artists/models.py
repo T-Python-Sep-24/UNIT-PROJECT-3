5fd3465a039d0397ed81e070d9a3123fd376667a
Ghaliah <ghalia.banouna@gmail.com>
2024-12-01 17:52:36 +0300

    Modification to artist Model and necessary changes in Templates

--- ArtGallery/artists/models.py
+++ ArtGallery/artists/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Artist(models.Model):
     fullName = models.CharField(max_length=1024)
     about = models.TextField()
-    birthDate = models.DateField()
     exhibit = models.CharField(max_length=1024)
+    instagram = models.URLField()
     photo = models.ImageField(upload_to="images/atrists/", default="images/usersPfps/defaultPfp.jpg")
     addedAt = models.DateTimeField(auto_now_add=True)
 
