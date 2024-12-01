95433a9a604aa0b080c38ebd8c20fb63bfb5add2
Eyad <eyalghamdi@outlook.com>
2024-12-02 01:22:08 +0300

    add superuser permissions

--- BeautiBook/accounts/models.py
+++ BeautiBook/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    role = models.OneToOneField(User ,on_delete=models.CASCADE)
+    about_user = models.TextField(blank=True)
+    is_artist = models.BooleanField(default=False)
+    def __str__(self):
+        return f"Profile {self.user.username}"
--- BeautiBook/accounts/views.py
+++ BeautiBook/accounts/views.py
@@ -3,14 +3,24 @@ from django.http import HttpRequest,HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import login,logout,authenticate 
 from django.contrib import messages
+from .models import Profile
 
 # Create your views here.
 def signup_view(request:HttpRequest):
    
     if request.method == "POST":
         try:
-            new_user = User.objects.create_user(username=request.POST["username"],password=request.POST["password"],first_name=request.POST["first_name"],email=request.POST["email"])
+            new_user = User.objects.create_user(
+                username=request.POST["username"],
+                password=request.POST["password"],
+                first_name=request.POST["first_name"],
+                email=request.POST["email"]
+                )
             new_user.save()
+            new_profile = Profile(role = new_user,# about_user = request.POST["about_user"],
+                                  is_artist = request.POST["is_artist"],
+            )
+            new_profile.save()
             messages.success(request,"Registered User Successfully","alert-success")
             return redirect("accounts:signin_view")
         except Exception as e:
@@ -34,3 +44,8 @@ def signin_view(request:HttpRequest):
 def logout_view(request:HttpRequest):
     logout(request)
     return redirect("main:home_view")
+
+def user_profile_view(request:HttpRequest,user_name):
+    user = User.objects.get(username=user_name)
+    profile:Profile = user.profile
+    return render(request,'accounts/profile.html',{"user":user})
