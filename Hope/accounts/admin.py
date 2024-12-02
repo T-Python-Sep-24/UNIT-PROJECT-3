805ab7a2ce52caa5cd477605e95779f59993382c
KhuloodGhazi <khulood.gmo@gmail.com>
2024-12-02 11:44:15 +0300

    Few modification & Done with README

--- Hope/accounts/admin.py
+++ Hope/accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import OrganizationProfile, VolunteerProfile
 
 # Register your models here.
+
+admin.site.register (OrganizationProfile)
+admin.site.register (VolunteerProfile)
