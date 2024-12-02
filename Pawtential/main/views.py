5b3fd8e0ffeb67777494dd643559673927a3ded1
alhanoufALLUHAYDDAN <alhanoufkhalaf1@gmail.com>
2024-12-02 13:18:29 +0300

    all completed

--- Pawtential/main/views.py
+++ Pawtential/main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from pets.models import Pet
+
 # Create your views here.
 
 def home_view(request):
-
-    return render(request, 'main/home.html')
+    pets = Pet.objects.all().order_by('-created_at')[:3]
+    
+    return render(request, 'main/home.html', {'pets': pets})
