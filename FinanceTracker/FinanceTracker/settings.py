810ca672e1e1c7f8efbb81533688522e6a6310f7
Fahad <fahad.alabdulkarim5@gmail.com>
2024-12-03 11:09:25 +0300

    FInanceTracker1.0

--- FinanceTracker/FinanceTracker/settings.py
+++ FinanceTracker/FinanceTracker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -142,6 +142,10 @@ CELERY_TASK_SERIALIZER = 'json'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- FinanceTracker/users/urls.py
+++ FinanceTracker/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),  
@@ -11,3 +12,6 @@ urlpatterns = [
     path('profile/<int:user_id>/', views.user_profile, name='profile'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
